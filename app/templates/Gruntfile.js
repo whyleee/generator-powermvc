883ebdd696c53470da639d4ce14e38e695fe383c
Pavel Nezhencev <whyleee@gmail.com>
2014-11-04 22:05:19 +0200

    Greatly improved grunt speed by using jit-grunt

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -12,8 +12,11 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+  // Load grunt tasks on demand
+  require('jit-grunt')(grunt, {
+    bower: 'grunt-bower-requirejs',
+    cdnify: 'grunt-google-cdn-implicit'
+  });
 
   // Configurable paths
   var config = {
