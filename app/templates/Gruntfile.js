59dbb334e1ffea6bf1d7f6f21292d5e14d8b4972
Pavel Nezhencev <whyleee@gmail.com>
2014-11-12 00:03:40 +0200

    Enabled css source maps

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -143,7 +143,8 @@ module.exports = function (grunt) {
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
-        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
+        map: true
       },
       dist: {
         files: [{
