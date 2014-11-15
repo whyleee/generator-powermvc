840d5bce91c5ccd328d278610728b3feba50bb88
Pavel Nezhencev <whyleee@gmail.com>
2014-11-15 17:25:23 +0200

    Adding mime types for scss/sass files for css source maps support in browsers

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ var path = require('path');
 var fs = require('fs');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var xmldom = require('xmldom');
+var xpath = require('xpath');
 
 module.exports = yeoman.generators.Base.extend({
   constructor: function () {
@@ -220,6 +222,16 @@ module.exports = yeoman.generators.Base.extend({
     this.write(this.projName + '.csproj', proj);
   },
 
+  webconfig: function () {
+    // add mime types for scss/sass files for css source maps support in browsers
+    this._addToConfig('/configuration/system.webServer/staticContent', [
+      {name: 'remove', attrs: {fileExtension: '.scss'}},
+      {name: 'mimeMap', attrs: {fileExtension: '.scss', mimeType: 'text/x-scss'}},
+      {name: 'remove', attrs: {fileExtension: '.sass'}},
+      {name: 'mimeMap', attrs: {fileExtension: '.sass', mimeType: 'text/x-sass'}}
+    ]);
+  },
+
   install: function () {
     this.on('end', function () {
       if (!this.options['skip-install']) {
@@ -259,5 +271,223 @@ module.exports = yeoman.generators.Base.extend({
       dev: Boolean(dev)
     });
     this.write(path, content);
+  },
+
+  _addToConfig: function(parent, elems) {
+    var changed = 0;
+    var configXml = this.readFileAsString('Web.config');
+    var configDom = new xmldom.DOMParser().parseFromString(configXml);
+
+    changed += this._safeAddXmlConfigNodes(configDom, parent, elems);
+
+    if (changed > 0) {
+      this._safeXmlWrite('Web.config', configDom, configXml);
+    }
+  },
+
+  _fixDocIndent: function(doc, xml) {
+    var rootComment = doc.documentElement.previousSibling;
+
+    if (rootComment && rootComment.nodeType == 8 /* COMMENT_NODE */) {
+      doc.insertBefore(doc.createTextNode('\r\n'), rootComment);
+      doc.insertBefore(doc.createTextNode('\r\n'), doc.documentElement);
+    }
+  },
+
+  _safeXmlWrite: function(path, doc, xml) {
+      this._fixDocIndent(doc, xml);
+
+      var xmlSerializer = new xmldom.XMLSerializer();
+      var updatedXml = xmlSerializer.serializeToString(doc);
+
+      // Fix serialization: use ' />' instead of '/>' if target config uses that style
+      var noSpaceClosesCount = (xml.match(/[^ ]\/>/g) || []).length;
+      var oneSpaceClosesCount = (xml.match(/ \/>/g) || []).length;
+
+      if (oneSpaceClosesCount > noSpaceClosesCount) {
+        updatedXml = updatedXml.replace(/([^ ])\/>/g, '$1 />');
+      }
+      
+      this.write(path, updatedXml);
+  },
+
+  _safeAddXmlConfigNodes: function(doc, parent, elems) {
+    var parentNode = doc.documentElement;
+    var node = null;
+    var indent = 1;
+
+    // get all required parent node names
+    var nodeNames = parent.replace('/' + parentNode.nodeName + '/', '').split('/');
+
+    // insert missing parent nodes
+    for (var i = 0; i < nodeNames.length; ++i) {
+      var nodeName = nodeNames[i];
+      node = parentNode.getElementsByTagName(nodeName)[0];
+
+      if (!node) {
+        node = doc.createElement(nodeName);
+        insertAlphabetically(parentNode, [node], indent, /*last*/false);
+      }
+
+      parentNode = node;
+      indent++;
+    }
+
+    var nodes = [];
+
+    // create all missing nodes to be added
+    for (var i = 0; i < elems.length; ++i) {
+      var elem = elems[i];
+      var existingNodes = parentNode.getElementsByTagName(elem.name);
+      var nodeExists = Array.prototype.slice.call(existingNodes)
+        .filter(function(node) { return sameAttrs(node, elem.attrs); })
+        .length > 0;
+
+      if (nodeExists) {
+        continue;
+      }
+
+      var node = doc.createElement(elem.name);
+
+      for (var attr in elem.attrs) {
+        node.setAttribute(attr, elem.attrs[attr]);
+      }
+
+      nodes.push(node);
+    }
+
+    // add missing nodes
+    if (nodes.length > 0) {
+      insertAlphabetically(parentNode, nodes, indent, /*last*/true);
+    }
+
+    return nodes.length;
+    // end
+
+    function sameAttrs(node, attrs) {
+      for (var attr in attrs) {
+        if (node.getAttribute(attr) != attrs[attr]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    function insertAlphabetically(parentNode, nodes, indent, isLast) {
+      var inserted = false;
+
+      // always insert last node to the end
+      if (!isLast) {
+        var siblings = parentNode.childNodes;
+        var nodeName = nodes[0].nodeName;
+
+        // if 'system.*' node: place together with other 'system.*' nodes
+        var isSystemNode = function(node) {
+          return node.nodeName.indexOf('system.') === 0;
+        };
+        var isSystemNodeToInsert = isSystemNode(nodes[0]);
+
+        if (isSystemNodeToInsert) {
+          var systems = [];
+
+          for (var i = 0; i < siblings.length; ++i) {
+            if (siblings[i].nodeName.indexOf('system.') === 0) {
+              systems.push(siblings[i]);
+            }
+          }
+
+          if (systems.length > 0) {
+            siblings = systems;
+          }
+        }
+
+        // find a place alphabetically and insert nodes
+        for (var i = 0; i < siblings.length; ++i) {
+          var isLastSibling = i == siblings.length - 1;
+          var isLastSystemSibling = isLastSibling && isSystemNodeToInsert;
+          var isNextSiblingNextAlphabetically = !isLastSibling
+            && siblings[i + 1].nodeName.localeCompare(nodeName) > 0;
+
+          if (isLastSystemSibling || isNextSiblingNextAlphabetically) {
+            var beforeNode = siblings[i].nextSibling;
+
+            // if has next sibling - place before indent (for correct formatting)
+            if (beforeNode && beforeNode.nodeType == 3 /* TEXT_NODE */) {
+              beforeNode = beforeNode.nextSibling;
+            }
+
+            insertNodes(parentNode, nodes, indent, isLast, beforeNode);
+            inserted = true;
+            break;
+          }
+        }
+      }
+
+      // if wasn't inserted yet: just add to the end
+      if (!inserted) {
+        insertNodes(parentNode, nodes, indent, isLast);
+      }
+    }
+
+    function insertNodes(parentNode, nodes, indent, isLast, beforeNode) {
+      // create indents
+      var indentBefore = doc.createTextNode(Array(indent + 1).join('  '));
+      var indentInner = doc.createTextNode('\r\n');
+      var indentAfter = doc.createTextNode('\r\n' + Array(indent).join('  '));
+      var createSiblingIndentNode = function() {
+        return doc.createTextNode('\r\n' + Array(indent + 1).join('  '));
+      }
+
+      if (beforeNode) {
+        // insert before with fixed indents
+        parentNode.insertBefore(indentAfter, beforeNode);
+        var siblingBeforeNode = indentAfter;
+
+        for (var i = nodes.length - 1; i >= 0; --i) {
+          parentNode.insertBefore(nodes[i], siblingBeforeNode);
+          siblingBeforeNode = nodes[i];
+
+          if (i > 0) {
+            var siblingIndentNode = createSiblingIndentNode();
+            parentNode.insertBefore(siblingIndentNode, siblingBeforeNode);
+            siblingBeforeNode = siblingIndentNode;
+          }
+        }
+        
+        parentNode.insertBefore(indentBefore, siblingBeforeNode);
+      } else {
+        // add to the end with fixed indents
+        parentNode.appendChild(indentBefore);
+
+        for (var i = 0; i < nodes.length; ++i) {
+          parentNode.appendChild(nodes[i]);
+
+          if (i < nodes.length - 1) {
+            parentNode.appendChild(createSiblingIndentNode());
+          }
+        }
+
+        parentNode.appendChild(indentAfter);
+      }
+
+      // if has prev sibling - decrease indent
+      var prevSibling = indentBefore.previousSibling;
+
+      if (prevSibling && prevSibling.nodeType == 3 /* TEXT_NODE */
+        && prevSibling.nodeValue.indexOf('  ') !== -1) {
+        indentBefore.deleteData(0, prevSibling.nodeValue.replace('\r\n', '').length);
+      }
+
+      // if has next sibling - increase indent
+      if (indentAfter.nextSibling) {
+        indentAfter.appendData('  ');
+      }
+
+      // if has children - add inner indent
+      if (!isLast) {
+        nodes[0].appendChild(indentInner);
+      }
+    }
   }
 });
